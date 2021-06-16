e9e8cae75c36cf5c8558b28d48097b96f92d24c7
Utkarshmishra04 <utkarsh75477@gmail.com>
2021-06-17 02:26:00 +0530

    added dmc

--- mopac/algorithms/mopac.py
+++ mopac/algorithms/mopac.py
@@ -36,7 +36,6 @@ class MOPAC(RLAlgorithm):
     def __init__(
             self,
             training_environment,
-            #changing_environment,
             evaluation_environment,
             policy,
             Qs,
@@ -128,7 +127,6 @@ class MOPAC(RLAlgorithm):
         self._writer = Writer(self._log_dir)
 
         self._training_environment = training_environment
-        #self._changing_environment = changing_environment
         self._evaluation_environment = evaluation_environment
         self._policy = policy
 
@@ -193,7 +191,6 @@ class MOPAC(RLAlgorithm):
             pool (`PoolBase`): Sample pool to add samples to
         """
         training_environment = self._training_environment
-        #changing_environment = self._changing_environment
         evaluation_environment = self._evaluation_environment
         policy = self._policy
         pool = self._pool
@@ -215,7 +212,6 @@ class MOPAC(RLAlgorithm):
 
         for self._epoch in gt.timed_for(range(self._epoch, self._n_epochs)):
             
-            #if self._epoch==0: training_environment = changing_environment
             self._epoch_before_hook()
             gt.stamp('epoch_before_hook')
 
